0afb9126ad91f1821e7ff41576320e03d829b968
Douglas Proença <douglasp.r.desouza@gmail.com>
2022-12-05 16:07:37 -0300

    Ajustando repositorio na nuvem

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Menu.Menu;
+
+/**
+ *
+ * @author Douglas
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.opcoes();
+    }
+}
